498f15b4605805fd92ec6e52c3107a29929766d7
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-28 17:48:57 -0400

    added event acknoledgements and states to form inputs

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,23 +1,44 @@
 const socket = io()
 
+//Elements
+const $messageaForm = document.querySelector("#message-form")
+const $messageaFormInput = $messageaForm.querySelector('input')
+const $messageaFormButton = $messageaForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message)=>{
     console.log(message)
 })
 
-document.querySelector("#message-form").addEventListener('submit', (e) => {
+$messageaForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    $messageaFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+
+    socket.emit('sendMessage', message, (error)=>{
+        $messageaFormButton.removeAttribute('disabled')
+        $messageaFormInput.value = ''
+        $messageaFormInput.focus()
+        if(error){
+            return console.log(error)
+        }
+        console.log('Sent!')
+    })
 })
 
-document.querySelector('#send-location').addEventListener('click', ()=>{
+$sendLocationButton.addEventListener('click', ()=>{
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser')
     }
     navigator.geolocation.getCurrentPosition((position)=>{
+        $sendLocationButton.setAttribute('disabled', 'disabled')
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () =>{
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared')
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express();
 const server = http.createServer(app)
@@ -19,12 +20,19 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message','A new user has joined')
     
-    socket.on('sendMessage', (sentMessage)=>{
+    socket.on('sendMessage', (sentMessage, callback)=>{
+        const filter = new Filter()
+
+        if(filter.isProfane(sentMessage)){
+            return callback('Profanity is not allowed')
+        }
         io.emit('message', sentMessage)
+        callback()
     })
 
-    socket.on('sendLocation', (sentLocation) => {
+    socket.on('sendLocation', (sentLocation, callback) => {
         io.emit('message', `https://google.com/maps?q=${sentLocation.latitude},${sentLocation.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', ()=>{
